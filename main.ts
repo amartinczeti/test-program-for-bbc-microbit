865e49e75db746757070470c69616b4d085f9d1b
amartinczeti <amartinczeti@outlook.com>
2024-05-30 09:32:27 +1000

    Made all LED checker go for longer (3000ms)

--- main.ts
+++ main.ts
@@ -1,25 +1,18 @@
 input.onPinPressed(TouchPin.P0, function () {
-    basic.showString("0")
+    basic.showString("P0")
 })
 input.onButtonPressed(Button.A, function () {
     basic.showString("A")
 })
 input.onPinPressed(TouchPin.P2, function () {
-    basic.showString("2")
-    basic.showLeds(`
-        . . . . #
-        . . . # .
-        # . # . .
-        . # . . .
-        . . . . .
-        `)
+    basic.showString("P2")
 })
 input.onButtonPressed(Button.AB, function () {
     input.calibrateCompass()
     basic.showLeds(`
         . . . . #
         . . . # .
-        # . # . .
+        # . . . .
         . # . . .
         . . . . .
         `)
@@ -28,14 +21,7 @@ input.onButtonPressed(Button.B, function () {
     basic.showString("B")
 })
 input.onPinPressed(TouchPin.P1, function () {
-    basic.showString("1")
-    basic.showLeds(`
-        . . . . #
-        . . . # .
-        # . # . .
-        . # . . .
-        . . . . .
-        `)
+    basic.showString("P1")
 })
 input.onGesture(Gesture.Shake, function () {
     basic.showArrow(ArrowNames.North)
@@ -71,7 +57,7 @@ basic.showLeds(`
     # # # # #
     # # # # #
     `)
-basic.pause(500)
+basic.pause(3000)
 basic.showLeds(`
     . . . . .
     . # . # .
